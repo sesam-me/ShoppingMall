be0ca4bedfe65ce1c42e4de98879d08c14cfc1d0
donymyounglee <pajang1515@daum.net>
2023-07-31 15:20:40 +0900

    error 처리

--- src/main/java/com/example/shopping/member/service/MemberService.java
+++ src/main/java/com/example/shopping/member/service/MemberService.java
@@ -1,5 +1,7 @@
 package com.example.shopping.member.service;
 
+import com.example.shopping.common.RestError;
+import com.example.shopping.common.RestResult;
 import com.example.shopping.member.domain.Response.MemberResponse;
 import com.example.shopping.member.domain.dto.*;
 import com.example.shopping.member.domain.entity.Grade;
@@ -9,19 +11,23 @@ import com.example.shopping.member.repository.GradeRepository;
 import com.example.shopping.member.repository.MemberRepository;
 import com.example.shopping.member.repository.PointRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
     private final PointRepository pointRepository;
     private final GradeRepository gradeRepository;
 
-    public void memberInsert(MemberInsertDto memberInsertDto){
+    public ResponseEntity<RestResult<Object>> memberInsert(MemberInsertDto memberInsertDto){
         // Grade Default 객체 생성 ( 5등급 )
         Grade grade = Grade.builder()
                 .gradeName("5등급")
@@ -38,6 +44,12 @@ public class MemberService {
         gradeRepository.save(grade);
         pointRepository.save(point);
 
+        Member member1 = memberRepository.findById(memberInsertDto.getId());
+
+        if (member1 == null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(new RestResult<>("error", new RestError("duplicate_id", "이미 사용중인 ID 입니다.")));
+        }
         // Member 객체 생성 및 데이터 설정
         Member member = Member.builder()
                 .id(memberInsertDto.getId())
@@ -48,13 +60,14 @@ public class MemberService {
                 .point(point) // 저장된 Point 엔티티를 설정
                 .build();
 
+
+
         // Member 엔티티를 저장하면 자동으로 Grade와 Point 엔티티도 저장됨
         memberRepository.save(member);
+        return ResponseEntity.ok(new RestResult<>("success", "회원가입이 완료 되었습니다."));
     }
     public List<MemberResponse> findAll(){
         List<Member> all = memberRepository.findAll();
-        System.out.println(all);
-
 
         return all.stream().map(MemberResponse::new).toList();
     }

7c78fe9d9eab86503bf8bd89cc67ef229766f727
donymyounglee <pajang1515@daum.net>
2023-08-01 15:44:22 +0900

    merge 전 임시

--- src/main/java/com/example/shopping/product/domain/dto/ProductDto.java
+++ src/main/java/com/example/shopping/product/domain/dto/ProductDto.java
@@ -9,11 +9,13 @@ public class ProductDto {
     private String hName;
     private String eName;
     private String imgUrl;
+    private String brand;
 
     public ProductDto(Product product) {
         this.productSeq = product.getProductSeq();
         this.hName = product.getHname();
         this.eName = product.getEname();
         this.imgUrl = product.getImgUrl();
+        this.brand = product.getBrand();
     }
 }

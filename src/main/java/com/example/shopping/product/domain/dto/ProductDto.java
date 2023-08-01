0537b5c5868ada5589844d48964790c31d15703c
Amy <wlgpek90@naver.com>
2023-08-01 15:31:44 +0900

    brand

--- src/main/java/com/example/shopping/product/domain/dto/ProductDto.java
+++ src/main/java/com/example/shopping/product/domain/dto/ProductDto.java
@@ -7,11 +7,16 @@ import lombok.Getter;
 public class ProductDto {
     private Long productSeq;
     private String name;
+    private String brand;
     private String imgUrl;
+
+
 
     public ProductDto(Product product) {
         this.productSeq = product.getProductSeq();
         this.name = product.getName();
+        this.brand = product.getBrand();
         this.imgUrl = product.getImgUrl();
+
     }
 }
